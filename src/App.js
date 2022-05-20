31cd117074c4f8cf587f5177d4b104dfc98b7a6d
abusalehmatul <saleh.matul@gmail.com>
2022-05-20 16:28:13 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  Modal,
+  RadioButton,
+  TextField,
+  TextContainer,
+  DisplayText,
+  Button
+} from "@shopify/polaris";
+export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false
+    }
+  }
+  render() {
+    return (
+      <div className='col-md-10 container'>
+        <div>
+          <Button primary onClick={() => { this.setState({ active: true }) }}>Add Audit</Button>
+        </div>
+        <div className='mt-5 mb-5'>
+          {this.state.active ? <div className='bg-white m-auto p-4 col-md-12 col-12'>
+            <div className='d-flex col-md-10'> 
+              
+            </div>
+            <hr></hr>
+            <div className='col-md-10 col-10 m-auto'>
+              <p>Site ID: 1</p>
+              <TextField label='Name' autoComplete="off"></TextField>
+              <TextField label='Juridiction/City/Rigion' autoComplete="off" multiline={2}></TextField>
+              <TextField label='Site Description' autoComplete="off" multiline={2}></TextField>
+              <div className='col-md-10 col-10 d-flex'>
+                <div className='col-md-6 col-12 me-5'>
+                  <TextField label='Latitude'></TextField>
+                </div>
+                <div className='col-md-6 col-12'>
+                  <TextField label='Latitude'></TextField>
+                </div>
+              </div>
+            </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          </div> : ""}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import { AppProvider, Page, Frame } from "@shopify/polaris";
+import '@shopify/polaris/styles.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +12,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <Frame>
+        <Page>
+          <App />
+        </Page>
+      </Frame>
+    </AppProvider>
   </React.StrictMode>
 );
 
